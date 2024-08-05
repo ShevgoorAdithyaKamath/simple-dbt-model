92027f002734b99c1e4e7ff42778e7390d3dbd0c
Shevgoor Adithya Kamath <kingdomofadithya@gmail.com>
2024-08-05 15:22:27 +0530

    fix

--- test_dbt_opy/models/example/copy3.sql
+++ test_dbt_opy/models/example/copy3.sql
@@ -2,5 +2,4 @@
 -- Use the `ref` function to select from other models
 {{ config(materialized='table') }}
 select *
-from {{ ref('test_dbt_opy', 'copy3') }}
-where id = 1123412341234
+from {{ ref('copy2') }}
